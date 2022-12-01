6fb16d4a38e7fcb5626a9e678072b677612d332b
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-12-01 07:25:04 -0500

    🎉 Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿int currentCalorieCount = 0;
+List<int> elves = new();
+
+foreach (string? s in System.IO.File.ReadAllLines("input.txt"))
+{
+    if (string.IsNullOrEmpty(s)) {
+        elves.Add(currentCalorieCount);
+        currentCalorieCount = 0;
+        continue;
+    }
+
+    currentCalorieCount += Int32.Parse(s);
+
+}
+
+int top3 = elves.OrderByDescending(e => e).Take(3).Sum(e => e);
+
+Console.WriteLine($"Sum of Top 3 is: {top3}");
