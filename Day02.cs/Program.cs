9e11bbe738168d17a87b3bf529f29928d25b7b6e
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-12-03 06:15:39 -0500

    🎉 Day02 - Part 1

---
+++ Day02.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿var lines = System.IO.File.ReadAllLines("input.txt").ToList();
+
+// First Index = Opponent, Second Index = Me
+int[, ] results = { { 4, 8, 3 }, { 1, 5, 9 }, { 7, 2, 6 } };
+
+
+Console.WriteLine(lines.Sum(l => CalcScore(l)));
+
+
+
+
+
+
+/////
+int CalcScore(string match) =>  results[
+        (int)ToAction(match.Split(" ")[0].Trim()[0]),
+        (int)ToAction(match.Split(" ")[1].Trim()[0])
+    ];
+
+Actions ToAction(char action) => action switch
+{
+    'A' or 'X' => Actions.Rock,
+    'B' or 'Y' => Actions.Paper,
+    'C' or 'Z' => Actions.Scissors
+};
+
+enum Actions
+{
+    Rock = 0,
+    Paper = 1,
+    Scissors = 2
+}
