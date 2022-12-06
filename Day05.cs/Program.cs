1de8ee38b52afc1bf58521b965773d1cbdc8ac22
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-12-05 21:41:49 -0500

    Day05

---
+++ Day05.cs/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+string[] stackDiagram = lines.Take(Array.IndexOf(lines, "") - 1).ToArray();
+string[] instructions = lines.Skip(Array.IndexOf(lines, "") + 1).ToArray();
+
+Stack<char>[] stacks = { new(), new(), new(), new(), new(), new(), new(), new(), new() };
+
+
+for (int l = stackDiagram.Length - 1; l >= 0; l--)
+{
+    string line = stackDiagram[l];
+    for (int j = 1; j < line.Length; j += 4)
+    {
+        int crateLocation = j;
+        int stackNumber = j / 4;
+        if (!char.IsWhiteSpace(line[crateLocation])) stacks[stackNumber].Push(line[crateLocation]);
+    }
+}
+
+foreach (string line in instructions)
+{
+    string[] instructionLines = line.Split(" ");
+    int crateCount = Convert.ToInt32(line.Split(" ")[1]);
+    int sourceStack = Convert.ToInt32(line.Split(" ")[3]);
+    int destinationStack = Convert.ToInt32(line.Split(" ")[5]);
+
+
+    var tempStack = new Stack<char>();
+
+    for (int i = 0; i < crateCount; i++)
+        tempStack.Push(stacks[sourceStack - 1].Pop());
+
+    for (int i = 0; i < crateCount; i++)
+        stacks[destinationStack - 1].Push(tempStack.Pop());
+
+}
+
+var result = string.Join("", stacks.Where(s => s.Count != 0).Select(s => s.Peek()));
+
+Console.WriteLine(result);
