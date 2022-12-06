2f597f57444a900a7ddfda8b47d8a6e07fb6ced4
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-12-06 07:29:21 -0500

    Day06

---
+++ Day06.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿string input = File.ReadAllText("input.example.txt");
+
+int marker = 14;
+
+for (int i = (marker - 1); i < input.Length; i++)
+{
+    if (input.Skip(i - (marker - 1)).Take(marker).Distinct().Count() == marker)
+    {
+        Console.WriteLine($"Characters: {i + 1}");
+        return;
+    }
+}
