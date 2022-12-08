84e1c541cef5ed7d4ffe9ab512c58b96fd6db082
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-12-08 16:20:01 -0500

    Day07 A

---
+++ Day07.cs/FileItem.cs
@@ -0,0 +1,7 @@
+public class FileItem
+{
+    public string ParentHierarchy { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Size { get; set; }
+
+}
--- Day07.cs/Program.cs
+++ Day07.cs/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+string currentDirectoryInHierarchy = "";
+List<FileItem> files = new();
+List<string> directories = new();
+
+for (int i = 0; i < lines.Length; i++)
+{
+    if (lines[i] == "$ ls") continue;
+    if (lines[i].StartsWith("dir")) continue;
+
+    if (lines[i] == "$ cd ..")
+    {
+        currentDirectoryInHierarchy = currentDirectoryInHierarchy.Substring(0, currentDirectoryInHierarchy.LastIndexOf("/"));
+        if (!directories.Contains(currentDirectoryInHierarchy)) directories.Add(currentDirectoryInHierarchy);
+        continue;
+    }
+
+    var lineData = lines[i].Split(" ");
+
+    if (lines[i].StartsWith("$ cd"))
+    {
+        currentDirectoryInHierarchy += "/" + lineData[2].Replace("/", ".");
+        if (!directories.Contains(currentDirectoryInHierarchy)) directories.Add(currentDirectoryInHierarchy);
+        continue;
+    }
+
+    files.Add(new()
+    {
+        ParentHierarchy = currentDirectoryInHierarchy,
+        Name = lineData[1],
+        Size = Convert.ToInt32(lineData[0])
+    });
+}
+
+var directorySizes = directories
+    .Select(d => new { Directory = d, Size = files.Where(f => f.ParentHierarchy.StartsWith(d)).Sum(f => f.Size) });
+
+
+Console.WriteLine(directorySizes.Where(d => d.Size <= 100000).Sum(d => d.Size));
+Console.WriteLine();
+
+// 847705 --> NO
