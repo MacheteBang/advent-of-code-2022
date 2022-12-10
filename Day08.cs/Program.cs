68ffe16825360aeb06a70de65c4633ad27939962
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-12-10 09:26:15 -0500

    Day 08 B

--- Day08.cs/Program.cs
+++ Day08.cs/Program.cs
@@ -4,6 +4,7 @@ int rowBound = forest.GetUpperBound(0);
 int colBound = forest.GetUpperBound(1);
 
 int visibleTreeCount = 0;
+int maxScenicScore = 0;
 
 // All trees on the perimeter are visible.
 visibleTreeCount += 2 * (colBound + 1); // top & bottom edges
@@ -21,15 +22,17 @@ for (int row = 1; row < rowBound; row++)
         var thisRowToWestEdge = Enumerable.Range(col + 1, colBound - col).Select(c => forest[row, c]).ToArray();
 
         var thisColToNorthEdge = Enumerable.Range(0, row).Select(r => forest[r, col]).ToArray();
-        Array.Reverse(thisRowToEastEdge);
+        Array.Reverse(thisColToNorthEdge);
 
         var thisRowToSouthEdge = Enumerable.Range(row + 1, rowBound - row).Select(r => forest[r, col]).ToArray();
 
         visibleTreeCount += IsVisible(thisTree, thisRowToEastEdge, thisColToNorthEdge, thisRowToWestEdge, thisRowToSouthEdge);
+        maxScenicScore = Math.Max(maxScenicScore, GetScenicScore(thisTree, thisRowToEastEdge, thisColToNorthEdge, thisRowToWestEdge, thisRowToSouthEdge));
     }
 }
 
 Console.WriteLine(visibleTreeCount);
+Console.WriteLine(maxScenicScore);
 
 
 
@@ -80,12 +83,33 @@ int IsVisible(int tree, int[] fromEast, int[] fromNorth, int[] fromWest, int[] f
     return 0;
 }
 
-int GetScenicScore(int tree, int[] treeList)
+int GetScenicScore(int tree, int[] fromEast, int[] fromNorth, int[] fromWest, int[] fromSouth)
 {
-    foreach (int s in treeList)
+    int e = 0, n = 0, w = 0, s = 0;
+
+    foreach (int t in fromEast)
     {
-        if (tree >= s) return s + 1;
+        e++;
+        if (t >= tree) break;
     }
 
-    return 0;
+    foreach (int t in fromNorth)
+    {
+        n++;
+        if (t >= tree) break;
+    }
+
+    foreach (int t in fromWest)
+    {
+        w++;
+        if (t >= tree) break;
+    }
+
+    foreach (int t in fromSouth)
+    {
+        s++;
+        if (t >= tree) break;
+    }
+
+    return e * n * w * s;
 }
